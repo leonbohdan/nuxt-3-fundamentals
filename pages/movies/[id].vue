2564f8d1f153305cdae5b057f0eed1e09764a404
bohdan leonets <bohdan@mytowerapp.com>
2023-03-19 23:11:25 +0200

    Working with Async Data in Nuxt

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -9,21 +9,41 @@ const movie = ref(null);
 //   movie.value = res;
 // };
 
-const { data } = useAsyncData(
-  `/movies/${route.params.id}`,
-  () => {
-    return $fetch(`${url.value}&i=${route.params.id}`);
-  },
+const { data, error } = await useFetch(
+  `${url.value}&i=${route.params.id}`,
+  // 'https://httpbin.org/status/500',
   {
-    pick: ['Plot', 'Title'],
-    // transform(data){
-    //   return {
-    //     Plot: data.Plot,
-    //     Title: data.Title,
-    //   };
-    // },
+    pick: ['Plot', 'Title', 'Error'],
+    key: `/movies/${route.params.id}`,
+    onResponseError(context) {
+      showError({statusCode: 500, statusMessage: 'Oh noes!'});
+    },
   },
 );
+
+if (error.value) {
+  console.log('error', error.value)
+}
+
+if (data.value.Error === 'Incorrect IMDb ID.') {
+  showError({statusCode: 404, statusMessage: 'Page not found!'});
+}
+
+// const { data } = useAsyncData(
+//   `/movies/${route.params.id}`,
+//   () => {
+//     return $fetch(`${url.value}&i=${route.params.id}`);
+//   },
+//   {
+//     pick: ['Plot', 'Title'],
+//     // transform(data){
+//     //   return {
+//     //     Plot: data.Plot,
+//     //     Title: data.Title,
+//     //   };
+//     // },
+//   },
+// );
 
 movie.value = data;
 </script>
