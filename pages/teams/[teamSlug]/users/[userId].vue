20ebe6adc9f756c1788ce0654736b9bb85fb5d53
bohdan leonets <bohdan@mytowerapp.com>
2023-03-19 21:45:53 +0200

    Routing in Nuxt

---
+++ pages/teams/[teamSlug]/users/[userId].vue
@@ -0,0 +1,16 @@
+<script setup>
+const route = useRoute();
+
+const message = computed(() => {
+  return `Team ${ route.params.teamSlug } and User id ${ route.params.userId }`;
+})
+</script>
+
+<template>
+  <p>
+<!--    Team {{ $route.params.teamSlug }}-->
+<!--    <br>-->
+<!--    User id {{ $route.params.userId }}-->
+    {{ message }}
+  </p>
+</template>
