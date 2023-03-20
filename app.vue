4bc7892f530b3bb2fc4920820b0c95ab5cafc92e
bohdan leonets <bohdan@mytowerapp.com>
2023-03-20 07:53:12 +0200

    Create Flexible Custom Layouts

--- app.vue
+++ app.vue
@@ -8,14 +8,8 @@ useHead({
 
 <template>
   <div>
-    <nav style="display: flex; gap: 10px">
-      <NuxtLink to="/">Home</NuxtLink>
-
-      <NuxtLink to="/movies">Movies</NuxtLink>
-
-      <NuxtLink to="https://vueschool.io/">Vue School</NuxtLink>
-    </nav>
-
-    <NuxtPage />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+<script setup>
+definePageMeta({
+  layout: 'plain',
+});
+</script>
+
+<template>
+  <form>
+    <h1>Login</h1>
+
+    <label for="">
+      Username
+      <input type="text">
+    </label>
+
+    <label for="">
+      Password
+      <input type="password">
+    </label>
+  </form>
+</template>
